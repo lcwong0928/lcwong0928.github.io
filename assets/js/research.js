2f4cd45fffce70e1d3972534d4d4dd9ba09bf02d
Lawrence Wong <lcwong0928@gmail.com>
2022-04-23 03:48:44 -0400

    Update research

--- assets/js/research.js
+++ assets/js/research.js
@@ -37,7 +37,7 @@ const research = [
     linkbox: "linkPopup2",
   },
 
-   {
+  {
     title: "Deep Learning for COVID-19 Diagnoses using Chest Imaging",
     authors:
       "William Phu, Lawrence Wong",
@@ -52,6 +52,40 @@ const research = [
     pdfbox: "pdfPopup3",
     link: "https://github.com/lcwong0928/covid-diagnosis",
     linkbox: "linkPopup3",
+  },
+
+  {
+    title: "Cancer Type Classification and Mortality Prediction",
+    authors:
+      "Jing Lin, Lawrence Wong, Maggie Wu, Yun Boyer",
+    conferences:
+      "6.439 Statistics, Computation and Applications",
+    researchYr: 2019,
+    image: "assets/images/research-page/6_439.png",
+    abstract:
+      "Abnormal gene expressions due to mutations in gene regulatory mechanisms result in the uncontrolled proliferation of cancer cells. Recent advances in machine learning as applied to gene expression profiling data have achieved great accuracy in the classification of cancer cell and have aided prediction on clinical outcomes for cancer patients. However, these prediction methods often deal with gene expression microarray datasets in high-dimensional space. Moreover, the time required to generate these datasets scales proportionally to the number of genes probed. We demonstrate using a logistic regression model and a 2D convolutional neural network that the classification accuracy will not suffer significantly when classifying with a selected subset of the gene expression data. This subset of genes is carefully selected by and verified with multiple feature selection techniques and extensive literature research on known driver genes. Subsequently, this subset is further utilized to predict the mortality of cancer patients using a ridge regression model and a convolutional neural network model. These results combined will help shed light on the mechanisms of gene regulation in cancer cells and set the stage better for future gene expression data analysis studies.",
+    absbox: "absPopup4",
+    pdf: "assets/docs/6_439.pdf",
+    pdfbox: "pdfPopup4",
+    link: "https://github.com/lcwong0928",
+    linkbox: "linkPopup4",
+  },
+
+  {
+    title: "Epigenetic Data Boosts the Accuracy of Recombination Hotspot Prediction by Machine Learning Models",
+    authors:
+      "Joy Linyue Fan, Lawrence Wong",
+    conferences:
+      "6.047 Computational Biology: Genomes, Networks, Evolution",
+    researchYr: 2019,
+    image: "assets/images/research-page/6_047.png",
+    abstract:
+      "Genetic recombination plays an integral role in generating genetic diversity in a population, but the mechanisms of the processes governing double-strand break (DSB) formation and subsequent ligation remain poorly understood. Recent advances in machine learning as applied to genetic data have demonstrated an ability to predict the location of recombination hotspots in the genome based on raw DNA sequences. However, these models neglect potential contributing factors from epigenetic marks and chromatin structure. Specifically, H3K4me3 and H3K36me3 are known to be correlated with the activity of PRDM9, a zinc finger protein that plays a role in determining sites of recombination in humans and mice, and open chromatin structure is required for the activity of the DSB-forming protein, Spo11. Furthermore, some correlation may exist between hotspot regions and SNP density. We demonstrate using simple classification models that the accuracy of hotspot prediction is significantly improved with the inclusion of ChIP-Seq epigenomic data, DNase hypersensitivity data, and Single Nucleotide Polymorphism (SNP) density data. A similar trend was observed in our deep learning model consisting of a hybrid deep convolutional and recurrent neural network trained on the new datasets as added features. This allowed us to produce a comprehensive predictive model for locations of hotspots in the human genome. Concurrently, we utilized the Gibbs sampling motif discovery technique in an attempt to discover binding motifs for Spo11 and PRDM9. These results combined will help shed light on the mechanisms of recombination and set the stage for better informed GWAS and linkage analysis studies.",
+    absbox: "absPopup5",
+    pdf: "assets/docs/6_047.pdf",
+    pdfbox: "pdfPopup5",
+    link: "https://github.com/lcwong0928",
+    linkbox: "linkPopup5",
   },
 
 ];
